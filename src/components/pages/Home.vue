70eee31d783ec24cc56280b031ded789c3d59853
matthieu brillaxis <matthieu brillaxis>
2017-06-08 14:39:40 +0200

    update cover

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -23,7 +23,7 @@
           title: '',
           artist: '',
           description: '',
-          img: 'https://www.outdoorresearch.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/244897_0936.jpg',
+          img: '',
           contributor: '',
         },
       };
